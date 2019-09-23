03419e06402e2ddb3c8b3c25c1ab85d0b89e4127
Administrator <Administrator@MICROSO-A9VQ8SQ>
2019-09-23 14:33:37 +0800

    create gitweb

---
+++ src/com/lxl/entity/Student.java
@@ -0,0 +1,31 @@
+package com.lxl.entity;
+
+/**
+ * @ClassName: Student
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author lxl
+ * @date 2019年9月23日
+ *
+ */
+public class Student {
+	private int stuIdl;
+	private String name;
+	
+	public void say(){
+		System.out.println("呵呵");
+	}
+	
+	public int getStuIdl() {
+		return stuIdl;
+	}
+	public void setStuIdl(int stuIdl) {
+		this.stuIdl = stuIdl;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
